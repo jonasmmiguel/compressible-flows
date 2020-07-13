432db9a521bd22da06ba1e67b0cce75d2d9aa3b7
Jonas M. Miguel <jonasmmiguel@gmail.com>
2020-07-13 17:45:50 -0300

    isentropic: now using full table for testing

---
+++ problems/assigments.py
@@ -0,0 +1,84 @@
+from src.workbench import isentropic as isen
+from src.workbench import nshock as ns
+from src.workbench import fanno as fn
+import numpy as np
+
+Runiv = 8.3145              # [J/mol.K]
+
+m_mol_air = 28.9645E-03     # [kg/mol]
+Rair = Runiv / m_mol_air    # [J/kg.K] gas constant for atmospheric air
+
+m_mol_n2 = 28E-03           # [kg/mol]
+Rn2 = Runiv / m_mol_n2      # [J/kg.K]
+
+m_mol_co2 = 48E-03          # [kg/mol]
+Rco2 = Runiv / m_mol_co2    # [J/kg.K]
+
+m_mol_co = 32E-03           # [kg/mol]
+Rco = Runiv / m_mol_co      # [J/kg.K]
+
+
+if __name__ == '__main__':
+    # === A3Q1 ===
+    # assumptions
+    k = 1.4
+    R = Rair
+
+    # given
+    M1 = 0.25
+    pt1 = 1.38E+06
+    Tt1 = 278
+    D = 0.0508
+    L = 0.6096
+    f = 0.024
+
+    # calculations
+    A = np.pi*(D**2)/4
+
+    T1 = isen('T', M=M1) * Tt1
+    p1 = isen('p', M=M1) * pt1
+    rho1 = p1/(R * T1)
+    u1 = M1 * (k * R * T1)**0.5
+
+    fld_2 = fn('fld', M=M1) - f*L/D
+    M2 = fn('M', fld=fld_2, regime='subsonic')
+    p2 = fn('p', M=M2) * (1 / fn('p', M=M1)) * (isen('p', M=M1)) * pt1
+    Tt2 = Tt1
+    T2 = Tt2 * isen('T', M=M2)
+    rho2 = p2/(R * T2)
+    u2 = M2 * (k * R * T2)**0.5
+
+    Ff = A*( (p2 + rho2 * u2**2) - (p1 + rho1 * u1**2))
+
+    # === A3Q2 ===
+    # assumptions
+    k = 1.4
+
+    # given
+    ARn = 2.5
+    L = 13xqayqasdcfasdfghjk
+    D = 1.5E-02
+    f = 0.016
+    pb = 45E+03
+
+    # both cases
+    Me = isen('M', A_ratio=ARn, regime='supersonic')
+
+    # case p0max
+    fld_e = fn('fld', M=Me)
+    fld_s = fld_e - f*L/D
+    Ms = fn('M', fld=fld_s, regime='supersonic')
+
+    # case p0min
+    Msl = ns('M', Ms=Me)
+    fld_sl = fn('fld', M=Msl)
+    fld_l = fld_sl - f*L/D
+    Ml = fn('M', fld=fld_l, regime='subsonic')
+    term1 = isen('p', M=Me) * ns('p', M=Me)
+    p0min = isen('p', M=Me) * ns('p', M=Me) * fn('p', M=Msl) * fn('p', M=Ml)
+
+
+
+
+
+    print('done')
--- problems/p2.py
+++ problems/p2.py
@@ -1,6 +1,6 @@
 from src.workbench import isentropic as isen
 from src.workbench import nshock as ns
-from src.workbench import fanno as f
+from src.workbench import fanno
 from src.workbench import rayleigh as r
 import numpy as np
 from scipy.optimize import minimize_scalar, minimize, fsolve, brentq
@@ -55,5 +55,57 @@ if __name__ == '__main__':
     # Tt2 = Tsl/isen('T', M=Msl)
     # q = 1*(Tt2 - Tt1)
 
-    
+    ##### Q1_D12
+    # Given
+    k=1.33
+    cp = 1.19
+    R = cp*(k-1)/(k)
+
+    pt1 = 3E+05
+    Tt1 = 800 + 273.15
+    pb = 1.013E+05
+
+    D2 = 0.5
+    D3 = 0.46
+    L = 10.0
+
+    # Calculations
+    # p3 = isen('p', k=1.33, M=1) * pt1
+    # is_blocked =  p3 > pb
+    # M3 = 1
+    #
+    # T3 = Tt1 * isen('T', k=1.33, M=1)
+    # mdot = M3 * ((k * R * T3) ** 0.5) * (p3 / (R * T3)) * (0.25 * np.pi * D3 ** 2)
+    #
+    # u3 = (k * R * T3) ** 0.5
+    #
+    # phi_A_M1 = isen('A', k=1.33, M=M3)
+    # M2 = isen('M', k=1.33, regime='subsonic', A_ratio=((D2 / D3) ** 2))
+    # M1 = M2
+    # T1 = Tt1 * isen('T', k=1.33, M=M1)
+    # u1 = (k * R * T1) ** 0.5
+    # F = mdot * (u3 - u1)
+
+    # Given
+    L = 10
+    f = 0.04
+    D = D2
+
+    # # Calculations
+    M3 = 1
+    T3 = Tt1 * isen('T', M=M3)
+    M2 = isen('M', k=1.33, regime='subsonic', A_ratio=(isen('A', k=1.33, M=M3) * (D2 / D3) ** 2))
+    fld2 = fanno('fld', M=M2)
+    fld1 = f*L/D + fld2
+    M1 = fanno('M', k=1.33, fld=fld1, regime='subsonic')
+    p1 = pt1 * isen('p', k=1.33, M=M1)
+    T1 = Tt1 * isen('T', k=1.33, M=M1)
+    p3 = isen('p', k=1.33, M=M3) * (1 / isen('p', k=1.33, M=M2)) * fanno('p', k=1.33, M=M2) * (1/fanno('p', k=1.33, M=M1)) * p1
+    is_blocked =  p3 > pb
+
+    mdot = M3 * ((k * R * T3) ** 0.5) * (p3 / (R * T3)) * (0.25 * np.pi * D3 ** 2)
+    u3 = (k * R * T3) ** 0.5
+    u1 = (k * R * T1) ** 0.5
+    F = mdot * (u3 - u1)
+
     print('done')
