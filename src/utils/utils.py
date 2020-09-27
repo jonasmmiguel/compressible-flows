403ac4d19472ed269ed3675c877f637baab5cfa2
Jonas M. Miguel <jonasmmiguel@gmail.com>
2020-09-27 01:04:30 -0300

    implemented steam interpolation functions

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,14 +1,22 @@
 import numpy as np
 
 
-def linint(x: float, p1: tuple, p2: tuple) -> float:
+def linint(x: float, point1: tuple, point2: tuple) -> float:
     """
     linear interpolation
     """
-    x1 = p1[0]
-    y1 = p1[1]
+    x1 = point1[0]
+    y1 = point1[1]
 
-    x2 = p2[0]
-    y2 = p2[1]
+    x2 = point2[0]
+    y2 = point2[1]
 
     return y1 + (x-x1) * (y2-y1) / (x2-x1)
+
+
+def get_vapor_property(x, qf, qg):
+    return linint(x, (0.0, qf), (1.0, qg))
+
+
+def get_quality(q, qf, qg):
+    return linint(q, (qf, 0.0), (qg, 1.0))
